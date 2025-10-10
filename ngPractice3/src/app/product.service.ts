cf9a7a43208590c13b44dce6fe53f9df7d29072d
Stuart Wong <swong31@gmail.com>
2025-10-09 21:24:29 -0700

    Trying to convert everything to observables

--- ngPractice3/src/app/product.service.ts
+++ ngPractice3/src/app/product.service.ts
@@ -1,15 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, pipe, tap } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private url: string = 'https://dummyjson.com/products';
+  products: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([])
 
   constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get(this.url);
+  getProducts(): Observable<any> {
+    return this.http.get(this.url).pipe(
+        tap((data: any)=> {
+            console.log('getProducts - Full response:', data);
+        console.log('getProducts - Products array:', data.products);
+        console.log('Is array?', Array.isArray(data.products));
+         this.products.next(data.products)
+        }    
+    ))
   }
+
+  addProduct(product: any): Observable<any> {
+    return this.http.post(this.url + '/add', + product)
+    // return this.http.post(don't know the url + input)
+  }
+
+  searchProduct(keyword: string) {
+    return this.http.get(this.url + '/search?q=' + keyword).pipe(tap((data: any)=> this.products.next(data.products)))
+  }
+
+//   manage behavior subject to emit new values when you add or delete products
 }
--- ngPractice3/src/app/products/products.component.ts
+++ ngPractice3/src/app/products/products.component.ts
@@ -1,49 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
+    arr$ = this.productService.getProducts()
+  errorMessage = '';
+  keyword = new FormControl();
+  subscriptions: Subscription[] = []
+  
   constructor(private productService: ProductService) {}
 
-    productsForm = new FormGroup({
-        title: new FormControl(),
-        description: new FormControl(),
-        category: new FormControl()
-    })
-  arr?: any[] = [];
-  errorMessage = '';
+  productsForm = new FormGroup({
+    title: new FormControl(),
+    description: new FormControl(),
+    category: new FormControl(),
+  });
+  
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe({
+    // Old Code
+    // this.productService.getProducts().subscribe({
+    //   next: (res: any) => {
+    //     this.arr = [...res.products];
+    //     console.log(this.arr);
+    //   },
+    //   error: (err: Error) => {
+    //     console.error('Error:', err.message);
+    //     this.errorMessage = err.message;
+    //   },
+    //   complete: () => console.log('Completed'),
+    // });
+    this.productService.products.subscribe(data => {
+    console.log('BehaviorSubject emitted:', data);
+    console.log('Is it an array?', Array.isArray(data));
+    console.log('Length:', data?.length);
+    console.log('First item:', data?.[0]);
+  });
+
+    // Loads initial products
+    this.subscriptions.push(this.productService.getProducts().subscribe())
+
+    // search as user types (with 1 second delay)
+    this.subscriptions.push(this.keyword.valueChanges.pipe(debounceTime(1000), switchMap((input)=> {
+        return this.productService.searchProduct(input)
+    })).subscribe({
       next: (res: any) => {
-        this.arr = [...res.products];
-        console.log(this.arr);
+        console.log(res)
       },
-      error: (err: Error) => {
-        console.error('Error:', err.message);
-        this.errorMessage = err.message;
-      },
-      complete: () => console.log('Completed'),
-    });
+    }))
   }
+
+  ngOnDestroy(): void {
+      this.subscriptions.forEach((sub:any)=> sub.unsubscribe())
+  }
+
+
 
   onSubmit(): void {
     if (this.productsForm.valid) {
-        const newProduct: any = {
-            id: (this.arr?.length ?? 0) + 2,
-            title: this.productsForm.value.title ?? "",
-            description: this.productsForm.value.description ?? "",
-            category: this.productsForm.value.category ?? ""
-        }
-        this.arr = [...(this.arr ?? []), newProduct]
-        this.productsForm.get('title')?.reset()
-        this.productsForm.get('description')?.reset()
-        this.productsForm.get('category')?.reset()
+      const newProduct: any = {
+        // id: (this.arr$.length ?? 0) + 2,
+        title: this.productsForm.value.title ?? '',
+        description: this.productsForm.value.description ?? '',
+        category: this.productsForm.value.category ?? '',
+      };
+      console.log(this.productService.products)
+    //   this.arr = [...(this.arr ?? []), newProduct];
+      this.productsForm.get('title')?.reset();
+      this.productsForm.get('description')?.reset();
+      this.productsForm.get('category')?.reset();
     }
   }
+  
 }
